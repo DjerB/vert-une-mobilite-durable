6ca0de8e708d091ca0e7af1ac55d1ecd06e0c242
DjerB <35849902+DjerB@users.noreply.github.com>
2019-10-13 22:55:42 +0100

    Delete app.config.js

--- src/app.config.js
+++
@@ -1,5 +0,0 @@
-export const DOMAIN = "vertunemobilitedurable";
-export const REGION = "eu-central-1";
-export const RESPONSE_TYPE = "token";
-export const CLIENT_ID = "74jqhkon51fhqcij088pashkgu";
-export const REDIRECT_URI = "https://vertunemobilitedurable.fr/onboarding";
